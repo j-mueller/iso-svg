495a7305b3419a45e673874f22e123aec928af43
Jann Mueller <j.mueller.11@ucl.ac.uk>
2016-07-26 21:07:54 +0100

    Polygons

--- examples/simple/Main.hs
+++ examples/simple/Main.hs
@@ -3,6 +3,7 @@ module Main where
 
 import Control.Lens hiding (children)
 import Graphics.Svg
+import Linear.V2
 
 import VirtualHom.Element
 import VirtualHom.Html hiding (content, main)
@@ -12,12 +13,13 @@ import VirtualHom.View(View, renderUI)
 
 aCircle :: Monad m => View m Int
 aCircle i = [svg
-  & attributes . at "viewBox" ?~ "0 0 120 120" 
+  & attributes . at "viewBox" ?~ "0 0 500 200" 
   & children .~ [
     circle 
       & attributes . at "cx" ?~ "60"
       & attributes . at "cy" ?~ "60"
-      & attributes . at "r" ?~ "50"
+      & attributes . at "r" ?~ "50",
+    polygon [V2 10 10, V2 20 20, V2 30 20]
     ]]
 
 main :: IO ()
--- src/Graphics/Svg.hs
+++ src/Graphics/Svg.hs
@@ -2,13 +2,18 @@
 module Graphics.Svg where
 
 import Control.Lens
+import Data.List (intercalate)
+import Data.Maybe (listToMaybe)
+import Data.Monoid
+import qualified Data.Text as T
 import Data.Text (Text)
+import Linear.V2
 import VirtualHom.Internal.Element (Elem, attributes, elm, namespace)
 
 xmlns :: Text
 xmlns = "http://www.w3.org/2000/svg"
 
--- | Creat an element in the SVG namespace
+-- | Create an element in the SVG namespace
 svgElm :: Text -> Elem cb ()
 svgElm t = elm t & namespace .~ xmlns
 
@@ -19,3 +24,15 @@ svg = svgElm "svg"
 
 circle :: Elem cb ()
 circle = svgElm "circle"
+
+path :: Elem cb ()
+path = svgElm "path"
+
+-- | Create a closed path from a list of points
+polygon :: Show a => [V2 a] -> Elem cb ()
+polygon pts = path & attributes . at "d" .~ thePath where
+  thePath = fmap makePoints $ listToMaybe pts
+  makePoints firstPoint = moveTo firstPoint <> foldMap lineTo (drop 1 pts) <> "z"
+  moveTo (V2 x y) = "M" <> (T.pack $ show x) <> "," <> (T.pack $ show y)
+  lineTo (V2 x y) = "L" <> (T.pack $ show x) <> "," <> (T.pack $ show y)
+    
